daac1d1a2f3feb5e81984ec9a32218fb9020df7b
Kristian <kris.dziamka@gmail.com>
2022-08-14 16:27:09 +0300

    ffff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
-import { fetchCountries } from './js/fetchCountries';
+import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
